c3f7d24e95797f7b48f543d648d4135261fe2949
Colde <pnw123456@icloud.com>
2024-10-19 23:05:41 +0900

    chore: sample code add

--- kotest-extensions-spring-restdocs-example/build.gradle.kts
+++ kotest-extensions-spring-restdocs-example/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("io.spring.dependency-management") version "1.1.6"
     kotlin("jvm") apply false
     kotlin("plugin.spring") version "1.9.24"
+    id("org.asciidoctor.jvm.convert") version "3.3.2" // SETTING TO SPRING
 }
 
 group = "com.github.xeounxzxu"
@@ -12,6 +13,11 @@ java {
     toolchain {
         languageVersion = JavaLanguageVersion.of(17)
     }
+}
+
+// SETTING TO SPRING
+configurations {
+    create("asciidoctorExt")
 }
 
 dependencies {
@@ -25,14 +31,17 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
-//    testImplementation("com.github.xeounxzxu:kotest-extensions-spring-restdocs:0253fa8280")
-    // implementation("com.github.xeounxzxu:kotest-extensions-spring-restdocs:0.0.2-SNAPSHOT")
+    testImplementation("com.github.xeounxzxu:kotest-extensions-spring-restdocs:0.0.0.3-SNAPSHOT")
 
     // kotest setting
     testImplementation("io.kotest:kotest-runner-junit5-jvm:${version.kotest}")
     testImplementation("io.kotest:kotest-assertions-core-jvm:${version.kotest}")
     testImplementation("io.kotest:kotest-property:${version.kotest}")
     testImplementation("io.kotest.extensions:kotest-extensions-spring:${version.extensionsSpring}")
+
+    // SETTING TO SPRING
+    "asciidoctorExt"("org.springframework.restdocs:spring-restdocs-asciidoctor:3.0.1")
+    testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc:3.0.1")
 }
 
 kotlin {
@@ -41,6 +50,21 @@ kotlin {
     }
 }
 
+// SETTING TO SPRING
+val snippetsDir by extra { file("build/generated-snippets") }
+
+tasks.test {
+    outputs.dir(snippetsDir)
+}
+
+tasks.named<org.asciidoctor.gradle.jvm.AsciidoctorTask>("asciidoctor") {
+    inputs.dir(snippetsDir)
+    configurations("asciidoctorExt")
+    dependsOn(tasks.test)
+}
+
 tasks.withType<Test> {
+    // SETTING TO SPRING
+    outputs.dir(snippetsDir)
     useJUnitPlatform()
 }
--- kotest-extensions-spring-restdocs-example/src/test/kotlin/com/github/xeounxzxu/kotestextensionsspringrestdocsexample/api/PingControllerTest.kt
+++ kotest-extensions-spring-restdocs-example/src/test/kotlin/com/github/xeounxzxu/kotestextensionsspringrestdocsexample/api/PingControllerTest.kt
@@ -1,20 +1,52 @@
 package com.github.xeounxzxu.kotestextensionsspringrestdocsexample.api
 
+import com.github.xeounxzxu.RestDocExtension
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.extensions.spring.SpringExtension
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.http.MediaType
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document
+import org.springframework.restdocs.payload.JsonFieldType
+import org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath
+import org.springframework.restdocs.payload.PayloadDocumentation.responseFields
+import org.springframework.restdocs.payload.ResponseFieldsSnippet
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.get
 
 
-class PingControllerTest : FunSpec() {
+@WebMvcTest(PingController::class)
+class PingControllerTest(
+    @Autowired val mockMvc: MockMvc
+) : FunSpec() {
 
     init {
-        extensions(SpringExtension)
+        extensions(SpringExtension, RestDocExtension)
 
         context("GET /api/ping") {
 
             test("정상적으로 ping 을 보낸다") {
-
-
+                mockMvc.get("/api/ping") {
+                    contentType = MediaType.APPLICATION_JSON
+                    accept = MediaType.APPLICATION_JSON
+                }.andExpect {
+                    status { isOk() }
+                }.andDo {
+                    document(
+                        "ping-controller-test",
+                        getPingResponse()
+                    )
+                }
             }
+        }
+    }
+
+    companion object {
+        fun getPingResponse(): ResponseFieldsSnippet {
+            return responseFields(
+                fieldWithPath("status")
+                    .type(JsonFieldType.STRING).description("상태"),
+            )
         }
     }
 }
