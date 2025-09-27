da0fa407774f3e0a680a60558b3c615bd10319a6
xeounxzxu <pnw123456@icloud.com>
2025-09-27 13:49:09 +0900

    fix: test case

--- kotest-spring-restdocs/src/test/kotlin/com/github/xeounxzxu/restdocs/RestDocsFieldsDslTest.kt
+++ kotest-spring-restdocs/src/test/kotlin/com/github/xeounxzxu/restdocs/RestDocsFieldsDslTest.kt
@@ -3,22 +3,18 @@ package com.github.xeounxzxu.restdocs
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.collections.shouldHaveSize
 import io.kotest.matchers.shouldBe
-import org.springframework.restdocs.payload.FieldDescriptor
 import org.springframework.restdocs.payload.JsonFieldType
 import org.springframework.restdocs.payload.SubsectionDescriptor
-import org.springframework.restdocs.snippet.Snippet
 
 class RestDocsFieldsDslTest : FunSpec({
 
     test("creates request field descriptors via DSL") {
-        val snippet = requestFields {
+        val descriptors = restDocsFieldDescriptors {
             "code" type JsonFieldType.STRING means "응답 코드"
             "message" type JsonFieldType.STRING optionalMeans "응답 메시지"
             "internal" type JsonFieldType.STRING ignoredMeans "내부 값"
             "data" subsection "응답 데이터"
         }
-
-        val descriptors = snippet.descriptors()
         descriptors.shouldHaveSize(4)
 
         descriptors[0].path shouldBe "code"
@@ -36,18 +32,13 @@ class RestDocsFieldsDslTest : FunSpec({
     }
 
     test("creates response descriptors with relaxed variant") {
-        val descriptors = relaxedResponseFields {
+        val descriptors = restDocsFieldDescriptors {
             "data" optionalSubsection "응답 데이터"
-        }.descriptors()
+        }
 
         descriptors.shouldHaveSize(1)
-        descriptors[0].isOptional shouldBe true
+        val subsection = descriptors[0] as SubsectionDescriptor
+        subsection.isOptional shouldBe true
+        subsection.path shouldBe "data"
     }
 })
-
-private fun Snippet.descriptors(): List<FieldDescriptor> {
-    val field = this::class.java.getDeclaredField("descriptors")
-    field.isAccessible = true
-    @Suppress("UNCHECKED_CAST")
-    return field.get(this) as List<FieldDescriptor>
-}
