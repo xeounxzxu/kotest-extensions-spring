7ea97fead2b8ded816cee84168f4fccfc751ed96
xeounxzxu <pnw123456@icloud.com>
2025-09-27 13:03:14 +0900

    fix: root project build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,7 @@
+import org.gradle.api.plugins.JavaPluginExtension
+import org.gradle.jvm.toolchain.JavaLanguageVersion
+import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
+
 plugins {
     val version = PluginsVersion
     id("org.jlleitschuh.gradle.ktlint") version version.ktlint
@@ -19,6 +23,16 @@ subprojects {
     apply(plugin = "org.jetbrains.kotlin.jvm")
     apply(plugin = "org.jetbrains.kotlin.kapt")
     apply(plugin = "org.jlleitschuh.gradle.ktlint")
+
+    extensions.configure<KotlinJvmProjectExtension>("kotlin") {
+        jvmToolchain(PluginsVersion.jvm.toInt())
+    }
+
+    extensions.configure<JavaPluginExtension> {
+        toolchain {
+            languageVersion.set(JavaLanguageVersion.of(PluginsVersion.jvm))
+        }
+    }
 
     dependencies {
         implementation("io.github.oshai:kotlin-logging-jvm:5.1.1")
